80d2c3d03b0e758e198fc46f02db0027076df8fc
svisuvas <svisuvas@gmail.com>
2020-06-12 09:21:06 -0400

    added application.properties user password instead of default"

--- src/main/java/io/webapi/demohelloworld/HelloResource.java
+++ src/main/java/io/webapi/demohelloworld/HelloResource.java
@@ -1,14 +1,27 @@
 package io.webapi.demohelloworld;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloResource {
 	
+	private String currentUser;
+	
 	@GetMapping("/")
 	public String hello() {
-		return "<h3>Hello World!</h3>";
+		
+		setCurrentUser(SecurityContextHolder.getContext().getAuthentication().getName());
+		return "<h3>Hello World! Welcome " +  this.currentUser + "</h3>";
+	}
+	
+	public String getCurrentUser() {
+		return this.currentUser;
+	}
+	
+	public void setCurrentUser(String currentUser) {
+		this.currentUser = currentUser;
 	}
 
 }
